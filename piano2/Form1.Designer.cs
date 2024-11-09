f1f1610a6c47f16269f41d50a7fe8853bef7c1f1
RubyWeng <ruby.weng777@gmail.com>
2024-11-09 15:20:20 +0800

    Initial commit of my WinForms project

---
+++ piano2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace piano2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnRe = new System.Windows.Forms.Button();
+            this.btnFa = new System.Windows.Forms.Button();
+            this.btnMi = new System.Windows.Forms.Button();
+            this.btnLa = new System.Windows.Forms.Button();
+            this.btnSo = new System.Windows.Forms.Button();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnPaint = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(243, 385);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(75, 25);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Do";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // btnRe
+            // 
+            this.btnRe.Location = new System.Drawing.Point(323, 385);
+            this.btnRe.Name = "btnRe";
+            this.btnRe.Size = new System.Drawing.Size(75, 25);
+            this.btnRe.TabIndex = 1;
+            this.btnRe.Text = "Re";
+            this.btnRe.UseVisualStyleBackColor = true;
+            this.btnRe.Click += new System.EventHandler(this.btnRe_Click);
+            // 
+            // btnFa
+            // 
+            this.btnFa.Location = new System.Drawing.Point(483, 385);
+            this.btnFa.Name = "btnFa";
+            this.btnFa.Size = new System.Drawing.Size(75, 25);
+            this.btnFa.TabIndex = 3;
+            this.btnFa.Text = "Fa";
+            this.btnFa.UseVisualStyleBackColor = true;
+            this.btnFa.Click += new System.EventHandler(this.btnFe_Click);
+            // 
+            // btnMi
+            // 
+            this.btnMi.Location = new System.Drawing.Point(403, 385);
+            this.btnMi.Name = "btnMi";
+            this.btnMi.Size = new System.Drawing.Size(75, 25);
+            this.btnMi.TabIndex = 2;
+            this.btnMi.Text = "Mi";
+            this.btnMi.UseVisualStyleBackColor = true;
+            this.btnMi.Click += new System.EventHandler(this.btnMi_Click);
+            // 
+            // btnLa
+            // 
+            this.btnLa.Location = new System.Drawing.Point(643, 385);
+            this.btnLa.Name = "btnLa";
+            this.btnLa.Size = new System.Drawing.Size(75, 25);
+            this.btnLa.TabIndex = 5;
+            this.btnLa.Text = "La";
+            this.btnLa.UseVisualStyleBackColor = true;
+            this.btnLa.Click += new System.EventHandler(this.btnLa_Click);
+            // 
+            // btnSo
+            // 
+            this.btnSo.Location = new System.Drawing.Point(563, 385);
+            this.btnSo.Name = "btnSo";
+            this.btnSo.Size = new System.Drawing.Size(75, 25);
+            this.btnSo.TabIndex = 4;
+            this.btnSo.Text = "So";
+            this.btnSo.UseVisualStyleBackColor = true;
+            this.btnSo.Click += new System.EventHandler(this.btnSo_Click);
+            // 
+            // btnSi
+            // 
+            this.btnSi.Location = new System.Drawing.Point(723, 385);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(75, 25);
+            this.btnSi.TabIndex = 6;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // btnPaint
+            // 
+            this.btnPaint.Location = new System.Drawing.Point(35, 388);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(75, 23);
+            this.btnPaint.TabIndex = 7;
+            this.btnPaint.Text = "Paint";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "小蜜蜂",
+            "小星星"});
+            this.comboBox1.Location = new System.Drawing.Point(29, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(90, 20);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(125, 33);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 9;
+            this.btnChoose.Text = "Choose";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(171, 319);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 452);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnPaint);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.btnLa);
+            this.Controls.Add(this.btnSo);
+            this.Controls.Add(this.btnFa);
+            this.Controls.Add(this.btnMi);
+            this.Controls.Add(this.btnRe);
+            this.Controls.Add(this.btnDo);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnRe;
+        private System.Windows.Forms.Button btnFa;
+        private System.Windows.Forms.Button btnMi;
+        private System.Windows.Forms.Button btnLa;
+        private System.Windows.Forms.Button btnSo;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnPaint;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ piano2/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piano2
+{
+    public partial class Form1 : Form
+    {
+        const int maxX = 800, maxY = 380, minX = 240, minY = 30;
+        int count = 0;
+        int[] arrbee = new int[49]
+                     {5,3,3,4,2,2,1,2,3,4,5,5,5,
+                      5,3,3,4,2,2,1,3,5,5,3,
+                      2,2,2,2,2,3,4,3,3,3,3,3,4,5,
+                      5,3,3,4,2,2,1,3,5,5,1};
+        int[] arrstar = new int[42]
+                     {1,1,5,5,6,6,5,4,4,3,3,2,2,1,
+                      5,5,4,4,3,3,2,5,5,4,4,3,3,2,
+                      1,1,5,5,6,6,5,4,4,3,3,2,2,1 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnPaint_Click(object sender, EventArgs e)
+        {
+            //draw(5, 3);
+        }
+        void draw(int[] arr ,int max)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+           
+            for (int i = 0; i < 8; i++)
+            {
+                g.DrawLine(new Pen(Color.Black),minX,minY+i*50,maxX,minY+i*50);
+                g.DrawLine(new Pen(Color.Black), minX + i * ((800-240) / 7), minY, minX + i * ((800-240) / 7), maxY);
+            }
+
+            //g.FillRectangle(new SolidBrush(Color.Black), 120 + (x-1) * (675 - 120) / 7, 30+(y-1)*40, (675 - 120) / 7, 40);
+
+            for (int i = 0,j=count; i<7; i++)
+            {
+                if (j>max-1) {  }
+                else
+                {
+                    if (j+i>max-1)
+                    {
+                        break;
+                    }
+                    g.FillRectangle(new SolidBrush(Color.Black), minX + ((arr[j + i]) - 1) * (800 - 240) / 7, minY + Math.Abs(i-6) * 50, (800 - 240) / 7, 50);                                   
+                }                    
+            }
+            if (count==max)
+            {
+                MessageBox.Show("恭喜你完成了演奏");
+                count = 0;
+            }            
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            richTextBox1.Text = "";
+            if (comboBox1.Text=="小蜜蜂")
+            {
+                draw(arrbee,49);
+            }
+            else if (comboBox1.Text=="小星星")
+            {
+                draw(arrstar, 42);
+            }
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D1:
+                    btnDo_Click(sender,e);
+                    break;
+                case Keys.D2:
+                    btnRe_Click(sender, e);
+                    break;
+                case Keys.D3:
+                    btnMi_Click(sender,e);
+                    break;
+                case Keys.D4:
+                    btnFe_Click(sender, e);
+                    break;
+                case Keys.D5:
+                    btnSo_Click(sender,e);
+                    break;
+                case Keys.D6:
+                    btnLa_Click(sender, e);
+                    break;
+                case Keys.D7:
+                    btnSi_Click(sender,e);
+                    break;
+            }
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            Console.Beep(262,500);
+            richTextBox1.Text += "Do";
+            if (comboBox1.Text=="小蜜蜂")
+            {
+                if (arrbee[count]==1)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text=="小星星")                
+            {
+                if (arrstar[count]==1)
+                {
+                    count++;
+                    draw(arrstar,42);
+                }
+            }
+        }
+        private void btnRe_Click(object sender, EventArgs e)
+        {
+            Console.Beep(294, 500);
+            richTextBox1.Text += "Re";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 2)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 2)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+        private void btnMi_Click(object sender, EventArgs e)
+        {
+            Console.Beep(330, 500);
+            richTextBox1.Text += "Mi";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 3)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 3)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+        private void btnFe_Click(object sender, EventArgs e)
+        {
+            Console.Beep(349, 500);
+            richTextBox1.Text += "Fa";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 4)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 4)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void btnSo_Click(object sender, EventArgs e)
+        {
+            Console.Beep(392, 500);
+            richTextBox1.Text += "So";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 5)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 5)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+        private void btnLa_Click(object sender, EventArgs e)
+        {
+            Console.Beep(440, 500);
+            richTextBox1.Text += "La";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 6)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 6)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            Console.Beep(494, 500);
+            richTextBox1.Text += "Si";
+            if (comboBox1.Text == "小蜜蜂")
+            {
+                if (arrbee[count] == 7)
+                {
+                    count++;
+                    draw(arrbee, 49);
+                }
+            }
+            else if (comboBox1.Text == "小星星")
+            {
+                if (arrstar[count] == 7)
+                {
+                    count++;
+                    draw(arrstar, 42);
+                }
+            }
+        }
+
+    }
+}
